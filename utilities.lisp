2b5c707e3166b1d0db1b3a9142b94228c0e360a3
Manish M Yathnalli <manish.ym@gmail.com>
2011-09-17 10:52:29 +0530

    Finished 4th chapter  the utilities

--- utilities.lisp
+++ utilities.lisp
@@ -142,3 +142,80 @@
                   ((= score max) 
                    (push obj result)))))
         (values (nreverse result) max))))
+
+(defun mapa-b (fn a b &optional (step 1))
+  (do ((i a (+ i step))
+       (result nil))
+      ((> i b) (nreverse result))
+    (push (funcall fn i) result)))
+
+(defun map-> (fn start test-fn succ-fn)
+  "Maping: map-> provides mechanism to map over arbitrary data
+            structures by providing options for functions to test and 
+            generate successive functions. test-fn is function that when true,
+            ends the map->. succ-fn is function used to generate next element"
+  (do ((i start (funcall succ-fn i))
+       (result nil))
+      ((funcall test-fn i) (nreverse result))
+    (push (funcall fn i) result)))
+
+(defun map0-n (fn n)
+  (mapa-b fn 0 n))
+
+(defun map1-n (fn n)
+  (mapa-b fn 1 n))
+
+(defun mapn-m (fn start end &optional step)
+  "mapn-m is simply mapa-b implemented using generic map-> function
+   to  demonstrate it"
+  (map-> fn start #'(lambda (x) (> x end)) #'(lambda (x) (+ x step))))
+
+(defun mappend (fn &rest lsts)
+  (apply #'append (apply #'mapcar fn lsts)))
+
+(defun mapcars (fn &rest lsts)
+  (let ((result nil))
+    (dolist (lst lsts)
+      (dolist (obj lst)
+        (push (funcall fn obj) result)))
+    (nreverse result)))
+
+(defun rmapcar (fn &rest args)
+  (if (some #'atom args)
+      (apply fn args)
+      (apply #'mapcar 
+             #'(lambda (&rest args)
+                 (apply #'rmapcar fn args))
+             args)))
+;; IO functions
+
+(defun readlist (&rest args)
+  (values (read-from-string
+           (concatenate 'string "(" (apply #'read-line args) ")"))))
+
+(defun prompt (&rest args)
+  (apply #'format *query-io* args)
+  (read *query-io*))
+
+(defun break-loop (fn quit &rest args)
+  (format *query-io* "Entering break-loop. ~%")
+  (loop
+     (let ((in (apply #'prompt args)))
+       (if (funcall quit in)
+           (return)
+           (format *query-io* "~A~%" (funcall fn in))))))
+
+(defun mkstr (&rest args)
+  (with-output-to-string (s)
+    (dolist (a args) (princ a s))))
+
+(defun explode (sym)
+  (map 'list #'(lambda (c)
+                 (intern (make-string 1 :initial-element c)))
+       (symbol-name sym)))
+
+(defun reread (&rest args)
+  (values (read-from-string (apply #'mkstr args))))
+
+(defun symb (&rest args)
+  (values (intern (apply #' mkstr args))))
